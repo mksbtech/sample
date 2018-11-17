636d10f0e54ed12998643a57ed39e2354c9e77b6
milan <mksbtech@gmail.com>
2018-11-17 15:57:16 +0530

    New files

---
+++ person-cloud-function/src/main/java/com/milan/entities/Person.java
@@ -0,0 +1,53 @@
+package com.milan.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)	
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+	
+	public Person()
+	{
+		
+	}
+	
+	public Person(String firstName, String lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
